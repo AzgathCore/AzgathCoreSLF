135ac00eba6210c2743bcb194b2936050cd68d1e
Aurøra/Nehyren <dreamcorerev@gmail.com>
2022-12-19 23:01:24 +0100

    Core: Fix build Ubuntu 22.04

--- src/server/worldserver/RemoteAccess/RASession.cpp
+++ src/server/worldserver/RemoteAccess/RASession.cpp
@@ -33,7 +33,7 @@ void RASession::Start()
 {
     // wait 1 second for active connections to send negotiation request
     for (int counter = 0; counter < 10 && _socket.available() == 0; counter++)
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        (std::chrono::milliseconds(100));
 
     // Check if there are bytes available, if they are, then the client is requesting the negotiation
     if (_socket.available() > 0)
